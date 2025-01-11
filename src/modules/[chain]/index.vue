dc088b33fdc4768cb3ae043224beda2f75a81e88
kek <0114kek@snu.ac.kr>
2025-01-11 16:16:37 +0900

    feat: unbond delegation in dashboard

--- src/modules/[chain]/index.vue
+++ src/modules/[chain]/index.vue
@@ -361,10 +361,24 @@ const amount = computed({
                     @click="dialog.open('delegate', { validator_address: item.delegation.validator_address }, updateState)">
                     {{ $t('account.btn_delegate') }}
                   </label>
-                  <label for="withdraw" class="btn !btn-xs !btn-primary btn-ghost rounded-sm"
+                  <label for="withdraw" class="btn !btn-xs !btn-primary btn-ghost rounded-sm mr-2"
                     @click="dialog.open('withdraw', { validator_address: item.delegation.validator_address }, updateState)">
                     {{ $t('index.btn_withdraw_reward') }}
                   </label>
+                  <label
+                    for="unbond"
+                    class="btn !btn-xs !btn-primary btn-ghost rounded-sm"
+                    @click="
+                      dialog.open(
+                        'unbond',
+                        {
+                          validator_address: item.delegation.validator_address,
+                        },
+                        updateState
+                      )
+                    "
+                    >{{ $t('account.btn_unbond') }}</label
+                  >
                 </div>
               </td>
             </tr>
