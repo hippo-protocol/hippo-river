9bb80a0abe08f4c2b56d5e3f22c5e88734d3148e
kek <0114kek@snu.ac.kr>
2025-05-26 14:49:44 +0900

    feat: add accounts tab, rich list

--- src/modules/[chain]/account/index.vue
+++ src/modules/[chain]/account/index.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
-import { computed, ref } from '@vue/reactivity';
-import { useBaseStore, useBlockchain, useFormatter } from '@/stores';
+import { ref } from '@vue/reactivity';
+import { useBlockchain, useFormatter } from '@/stores';
 import { PageRequest, type AuthAccount, type Pagination } from '@/types';
 import { onMounted } from 'vue';
 import PaginationBar from '@/components/PaginationBar.vue';
@@ -11,67 +11,73 @@ const chainStore = useBlockchain()
 const accounts = ref([] as AuthAccount[])
 const pageRequest = ref(new PageRequest())
 const pageResponse = ref({} as Pagination)
+const tab = ref('0')
 
 onMounted(() => {
-  pageload(1)
+    pageload(1)
 });
 
 function pageload(p: number) {
-  pageRequest.value.setPage(p)
-  chainStore.rpc.getAuthAccounts(pageRequest.value).then(x => {
-    accounts.value = x.accounts
-    pageResponse.value = x.pagination
-  });
+    pageRequest.value.setPage(p)
+    chainStore.rpc.getAuthAccounts(pageRequest.value).then(x => {
+        accounts.value = x.accounts
+        pageResponse.value = x.pagination
+    });
 }
 
-function showType(v: string) {
-    return v.replace("/cosmos.auth.v1beta1.", "")
-}
 function findField(v: any, field: string) {
-    if(!v || Array.isArray(v) || typeof v === 'string') return null
+    if (!v || Array.isArray(v) || typeof v === 'string') return null
     const fields = Object.keys(v)
-    if(fields.includes(field)) {
+    if (fields.includes(field)) {
         return v[field]
     }
-    for(let i= 0; i < fields.length; i++) {
+    for (let i = 0; i < fields.length; i++) {
         const re: any = findField(v[fields[i]], field)
-        if(re) return re
+        if (re) return re
     }
 }
 function showAddress(v: any) {
     return findField(v, 'address')
 }
-function showAccountNumber(v: any) {
-    return findField(v, 'account_number')
-}
-function showSequence(v: any) {
-    return findField(v, 'sequence')
-}
-function showPubkey(v: any) {
-    return findField(v, 'pub_key')
+
+const changeTab = (_tab: string) => {
+    tab.value = _tab
 }
 
 </script>
 <template>
     <div class=" overflow-x-auto">
+        <div class="tabs tabs-boxed bg-transparent mb-4 text-center flex justify-between gap-4">
+            <div>
+                <a class="tab text-gray-400 uppercase" :class="{ 'tab-active': tab === '0' }"
+                    @click="changeTab('0')">Rich List</a>
+            </div>
+        </div>
         <table class="table table-compact">
             <thead>
                 <tr>
-                    <td>{{ $t('account.type') }}</td>
+                    <td>Rank</td>
                     <td>{{ $t('account.address') }}</td>
-                    <td>{{ $t('account.acc_num') }}</td>
-                    <td>{{ $t('account.sequence') }}</td>
-                    <td>{{ $t('account.pub_key') }}</td>
+                    <td>Amount</td>
                 </tr>
             </thead>
             <tr v-for="acc in accounts">
-                <td>{{ showType(acc['@type']) }}</td>
-                <td><RouterLink :to="`/${chain}/account/${showAddress(acc)}`">{{ showAddress(acc) }}</RouterLink></td>
-                <td>{{ showAccountNumber(acc) }}</td>
-                <td>{{ showSequence(acc) }}</td>
-                <td>{{ showPubkey(acc) }}</td>
+                <td># 1</td>
+                <td>
+                    <RouterLink :to="`/${chain}/account/${showAddress(acc)}`">{{ showAddress(acc) }}</RouterLink>
+                </td>
+                <td> 123 HP </td>
             </tr>
         </table>
         <PaginationBar :limit="pageRequest.limit" :total="pageResponse.total" :callback="pageload" />
     </div>
 </template>
+
+<route>
+    {
+      meta: {
+        i18n: 'account',
+        order: 6
+      }
+    }
+  </route>
