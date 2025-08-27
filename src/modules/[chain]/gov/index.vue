b00e745f1db7816f04e95730803527fe8e011020
0114kek <0114kek@snu.ac.kr>
2025-08-27 16:26:52 +0900

    feat: add community pool spend proposal

--- src/modules/[chain]/gov/index.vue
+++ src/modules/[chain]/gov/index.vue
@@ -10,6 +10,7 @@ const tab = ref('0');
 const store = useGovStore();
 const pageRequest = ref(new PageRequest());
 const dialog = useTxDialog();
+const proposeModalRef = ref<HTMLDialogElement | null>(null);
 
 onMounted(() => {
   store.fetchProposals('0').then((x) => {
@@ -37,28 +38,67 @@ const proposeCallback = () => {
   store.fetchProposals('0');
   store.fetchProposals('1');
   store.fetchProposals('2');
-}
+};
 
+const openProposeModal = () => {
+  proposeModalRef.value?.showModal();
+};
+
+const closeProposeModal = () => {
+  proposeModalRef.value?.close();
+};
 
 </script>
 <template>
   <div>
-    <ActiveProposals type="gov"/>
-    <div class="h-[1px] w-full bg-[#2c3443] mb-[62px] mt-[17px]" ></div>
+    <ActiveProposals type="gov" />
+    <div class="h-[1px] w-full bg-[#2c3443] mb-[62px] mt-[17px]"></div>
     <div class="tabs tabs-boxed bg-transparent text-center flex justify-between mb-[48px] px-[40px]">
       <div class="flex gap-[4px]  rounded-[24px] border border-[#2c3443]  p-[4px]">
-        <a class="tab text-white text-normal capitalize !rounded-[20px]" :class="{ 'tab-active !text-black !bg-white': tab === '0' }" @click="changeTab('0')">All</a>
-        <a class="tab text-white text-normal capitalize !rounded-[20px]" :class="{ 'tab-active !text-black !bg-white': tab === '1' }"
-          @click="changeTab('1')">Deposit</a>
-        <a class="tab text-white text-normal capitalize !rounded-[20px]" :class="{ 'tab-active !text-black !bg-white': tab === '2' }" @click="changeTab('2')">{{
-          $t('gov.voting') }}</a>
-        <a class="tab text-white text-normal capitalize !rounded-[20px]" :class="{ 'tab-active !text-black !bg-white': tab === '3' }" @click="changeTab('3')">{{
-          $t('gov.passed') }}</a>
-        <a class="tab text-white text-normal capitalize !rounded-[20px]" :class="{ 'tab-active !text-black !bg-white': tab === '4' }" @click="changeTab('4')">{{
-          $t('gov.rejected') }}</a>
+        <a class="tab text-white text-normal capitalize !rounded-[20px]"
+          :class="{ 'tab-active !text-black !bg-white': tab === '0' }" @click="changeTab('0')">All</a>
+        <a class="tab text-white text-normal capitalize !rounded-[20px]"
+          :class="{ 'tab-active !text-black !bg-white': tab === '1' }" @click="changeTab('1')">Deposit</a>
+        <a class="tab text-white text-normal capitalize !rounded-[20px]"
+          :class="{ 'tab-active !text-black !bg-white': tab === '2' }" @click="changeTab('2')">{{
+            $t('gov.voting') }}</a>
+        <a class="tab text-white text-normal capitalize !rounded-[20px]"
+          :class="{ 'tab-active !text-black !bg-white': tab === '3' }" @click="changeTab('3')">{{
+            $t('gov.passed') }}</a>
+        <a class="tab text-white text-normal capitalize !rounded-[20px]"
+          :class="{ 'tab-active !text-black !bg-white': tab === '4' }" @click="changeTab('4')">{{
+            $t('gov.rejected') }}</a>
       </div>
-      <label for='propose' class="btn btn-secondary btn-sm !text-black !bg-white !rounded-[20px] !px-[20px] !py-[7px]"
-        @click="dialog.open('propose', {}, proposeCallback)">Propose</label>
+
+      <button type="button" class="btn btn-secondary btn-sm !text-black !bg-white !rounded-[20px] !px-[20px] !py-[7px]"
+        @click="openProposeModal">
+        Propose
+      </button>
+      <dialog id="proposeWrapperModal" ref="proposeModalRef" class="modal">
+        <div class="modal-box">
+          <h3 class="font-bold text-lg mb-4">Select Proposal Type</h3>
+          <div class="flex flex-col gap-[4px]"><label for='propose'
+              class="btn btn-secondary btn-sm !text-black !bg-white !rounded-[20px] !px-[20px] !py-[7px]"
+              @click="dialog.open('propose', {}, proposeCallback); closeProposeModal()">
+              Text Proposal
+            </label>
+            <label for='community_pool_spend'
+              class="btn btn-secondary btn-sm !text-black !bg-white !rounded-[20px] !px-[20px] !py-[7px]"
+              @click="dialog.open('community_pool_spend', {}, proposeCallback); closeProposeModal()">
+              Community Pool Spend Proposal
+            </label>
+          </div>
+
+          <div class="modal-action">
+            <button class="btn btn-ghost" @click="closeProposeModal">
+              Close
+            </button>
+          </div>
+        </div>
+        <form method="dialog" class="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
     </div>
     <ProposalListItem :proposals="store?.proposals[tab]" :tab="tab" />
     <PaginationBar :total="store?.proposals[tab]?.pagination?.total" :limit="pageRequest.limit"
